4b954813b9b7c9dacd6da491d1f96ca4bf93ccfe
José Manuel Barroso Galindo <theypsilon@gmail.com>
2020-06-24 11:47:58 +0200

    wip

--- dont_download.sh
+++ dont_download.sh
@@ -924,12 +924,12 @@ ini_settings_files_to_save() {
     INI_SETTINGS_FILES_TO_SAVE_RET_ARRAY=()
     for file in ${!SELECTED_INI_FILES[@]} ; do
         if [ -s "${SELECTED_INI_FILES[${file}]}" ] ; then
-            if ! diff "${file}" "${SELECTED_INI_FILES[${file}]}" > /Scripts/diff-log 2>&1 ; then
+            if ! diff "${file}" "${SELECTED_INI_FILES[${file}]}" > /media/fat/Scripts/diff-log 2>&1 ; then
                 INI_SETTINGS_FILES_TO_SAVE_RET_TEXT="${INI_SETTINGS_FILES_TO_SAVE_RET_TEXT}"$'\n'"${file}"
                 INI_SETTINGS_FILES_TO_SAVE_RET_ARRAY+=("${file}")
             fi
-            echo "One went" >> /Scripts/diff-log
-            echo >> /Scripts/diff-log
+            echo "One went" >> /media/fat/Scripts/diff-log
+            echo >> /media/fat/Scripts/diff-log
         fi
     done
 }
